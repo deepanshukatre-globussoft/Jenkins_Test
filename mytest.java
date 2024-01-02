badeaab6380459a1b02037a68ab7bb7baf4d73b6
deepanshukatre-globussoft <138182417+deepanshukatre-globussoft@users.noreply.github.com>
2024-01-02 17:32:05 +0530

    First commit

---
+++ mytest.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class mytest{
+    public static void main(String args[]){
+        System.out.println("This is Hello world program1");
+        System.out.println("This is Hello world program");
+        System.out.println(new Date());
+    }
+}
